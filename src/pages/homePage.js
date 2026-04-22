bd7a05311f568b1596eb365f2d4300263e8a7415
RyoNakajima421 <rnk110105@gmail.com>
2026-04-22 20:19:59 +0900

    Update homePage.js

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -9,9 +9,9 @@ export const homePage = {
       <section class="quick-grid quick-grid--home">
         ${ renderQuickLink('マップ', '/map', '地図から等aaaaaaaaaの検索が可能です。', "assets/img/map.png")}
         ${renderQuickLink('企画', '/projects', '全ての企画から絞り込み検索が可能です。', "assets/img/attractions.png")}
-        ${renderQuickLink('ステージ公演', '/stage', '公演の時間と場所の確認をすることができます。', "assets/img/callender.png")}
+        ${renderQuickLink('ステージ公演', '/stage', '公演の時間と場所の確認をすることができます。', "assets/img/event.png")}
         ${renderQuickLink('飲食', '/food', 'おいしい。', "assets/img/food.png")}
-        ${renderQuickLink('お気に入り', '/favorites', '保存した企画', "assets/img/event.png")}
+        ${renderQuickLink('お気に入り', '/favorites', '保存した企画', "assets/img/callender.png")}
       </section>
     `;
   },
